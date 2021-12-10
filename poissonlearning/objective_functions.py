Fixed error in gradient

poissonlearning/objective_functions.py
@@ -3,16 +3,20 @@ import scipy.sparse as spsparse
 
 
 def objective_p_laplace(u, W, b, p):
+    """
+    """
     difference_matrix = np.abs(u[:, np.newaxis] - u[np.newaxis, :]) ** p
     Ju = 0.5 / p * W.multiply(difference_matrix).sum() - np.dot(u, b)
     return Ju
 
 
 def objective_p_laplace_gradient(u, W, b, p):
+    """
+    """
     A = W.multiply(np.abs(u[:, np.newaxis] - u[np.newaxis, :]) ** (p - 2))
     # TODO: use get_node_degrees here?
     D = spsparse.diags(A.sum(axis=1).A1)
-    grad_Ju = 0.5 * (D - A) @ u - b
+    grad_Ju = 1.0 * (D - A) @ u - b
     return grad_Ju
 
 

tests/test_objective_functions.py
@@ -130,9 +130,7 @@ def test_objective_p_laplace_gradient(u, W, b, p):
     expected = np.zeros(n)
     for i in range(n):
         for j in range(n):
-            expected[i] += (
-                0.5 * W[i, j] * np.abs(u[i] - u[j]) ** (p - 2) * (u[i] - u[j])
-            )
+            expected[i] += W[i, j] * np.abs(u[i] - u[j]) ** (p - 2) * (u[i] - u[j])
 
         expected[i] -= b[i]
 