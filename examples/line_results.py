Fixed all scaling issues

examples/line_results.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import scipy.optimize
 import logging
 
 import matplotlib.pyplot as plt
@@ -69,7 +68,7 @@ if __name__ == "__main__":
                 experiments,
             )
         )[0]
-        for n in [10000, 20000, 50000, 100000]
+        for n in [10000, 20000, 50000]  # , 100000]
     }
 
     fig_dirac, ax_dirac = plt.subplots(1, 1)
@@ -85,7 +84,7 @@ if __name__ == "__main__":
     ax_dirac.grid(linestyle="dashed")
 
     # Convergence of bumps
-    n = 100000
+    n = 50000
     kernel = "uniform"
     ex_bumps = {
         f"bump width={bump}": list(
@@ -118,20 +117,7 @@ if __name__ == "__main__":
         for kernel in ["uniform", "gaussian"]
     }
     fig_error, ax_error = plt.subplots(1, 1)
-    plotting.error_plot(ex_error, ax_error)
-
-    # Fit exponential error curve
-    def _func_exp(x, a, c):
-        return a * np.exp(-c * x)
-
-    x = [e["n"] for e in ex_error["uniform"]]
-    y = [e["err_mean"] for e in ex_error["uniform"]]
-    popt, pcov = scipy.optimize.curve_fit(_func_exp, x, y, p0=(1, 1e-5))
-    LOGGER.info("popt: {}".format(popt))
-
-    xplot = np.linspace(np.min(x), np.max(x), 1000)
-    yplot = _func_exp(xplot, *popt)
-    ax_error.plot(xplot, yplot, c="red", ls="--")
+    plotting.error_plot(ex_error, ax_error, fit="exponential")
 
     # ax_error.set_xscale("log")
     ax_error.set_title("L1 error of solution with dirac RHS")

examples/one_circle.py
@@ -88,9 +88,9 @@ def run_trial(seed):
         dataset = pl.datasets.Dataset(data[:n], labels[:n], metric="raw")
         d = dataset.data.shape[1]
 
-        sigma = utils.get_normalization_constant(experiment["kernel"], d, p)
-        rho2 = np.pi * np.pi  # Density of the probability distribution
-        scale = (sigma / rho2) * experiment["eps"] ** 2 * n ** 2
+        sigma = utils.get_normalization_constant(experiment["kernel"], d)
+        rho2 = 1.0 / (np.pi * np.pi)  # Density of the probability distribution
+        scale = 0.5 * sigma * rho2 * experiment["eps"] ** 2 * n ** 2
         solution, indices_largest_component = utils.run_experiment_poisson(
             dataset, experiment, scale, tol=1e-8,
         )

examples/one_circle_results.py
@@ -39,33 +39,14 @@ def compute_errors(experiments):
 
             mask_infty = np.isfinite(analytic)
 
-            error_L1_unscaled = np.abs(z[mask_infty] - analytic[mask_infty]).mean()
-            result["L1_unscaled"] = error_L1_unscaled
-
-            scale = (analytic[mask_infty] / z[mask_infty]).mean()
-            LOGGER.info(f"scale for {experiment['n']}:{result['seed']}: {scale}")
-            z_scaled = scale * z
-            error_L1_scaled = np.abs(z_scaled[mask_infty] - analytic[mask_infty]).mean()
-            result["L1_scaled"] = error_L1_scaled
+            error_L1 = np.abs(z[mask_infty] - analytic[mask_infty]).mean()
+            result["err_L1"] = error_L1
 
     for experiment in experiments:
-        L1_errors_unscaled = [r["L1_unscaled"] for r in experiment["results"]]
-        experiment["err_unscaled_mean"] = np.mean(L1_errors_unscaled)
-        experiment["err_unscaled_upper"] = (
-            np.max(L1_errors_unscaled) - experiment["err_unscaled_mean"]
-        )
-        experiment["err_unscaled_lower"] = experiment["err_unscaled_mean"] - np.min(
-            L1_errors_unscaled
-        )
-
-        L1_errors_scaled = [r["L1_scaled"] for r in experiment["results"]]
-        experiment["err_scaled_mean"] = np.mean(L1_errors_scaled)
-        experiment["err_scaled_upper"] = (
-            np.max(L1_errors_scaled) - experiment["err_scaled_mean"]
-        )
-        experiment["err_scaled_lower"] = experiment["err_scaled_mean"] - np.min(
-            L1_errors_scaled
-        )
+        L1_errors = [r["err_L1"] for r in experiment["results"]]
+        experiment["err_mean"] = np.mean(L1_errors)
+        experiment["err_upper"] = np.max(L1_errors) - experiment["err_mean"]
+        experiment["err_lower"] = experiment["err_mean"] - np.min(L1_errors)
 
 
 if __name__ == "__main__":
@@ -116,15 +97,8 @@ if __name__ == "__main__":
         for kernel in ["gaussian"]
     }
     fig_error, ax_error = plt.subplots(1, 1)
-    plotting.error_plot(
-        ex_error,
-        ax_error,
-        err_name="err_unscaled_mean",
-        err_lower_name="err_unscaled_lower",
-        err_upper_name="err_unscaled_upper",
-    )
+    plotting.error_plot(ex_error, ax_error, fit="polynomial")
     ax_error.set_title(f"L1 Error compared with RHS {bump_width} to analytic solution")
-    ax_error.grid()
     ax_error.legend()
 
     if SHOW_PLOTS:

poissonlearning/datasets/one_circle.py
@@ -23,7 +23,7 @@ def generate(center, r, size, rng=None):
 def greens_function(x, z):
     """See Benedikt Wirth (2020) "Green's Function for the Neumann–Poisson Problem on n-Dimensional Balls", 
     The American Mathematical Monthly, 127:8, 737-743, DOI: 10.1080/00029890.2020.179091"""
-    w2inv = 2 / np.pi
+    w2inv = 1 / np.pi
     z_norm = np.linalg.norm(z)
 
     with np.errstate(divide="ignore"):